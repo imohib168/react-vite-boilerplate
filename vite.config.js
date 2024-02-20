ae493ddb200ab0b0ae9c1facf893a69ba2dc6ecf
imohib168 <imohib168@gmail.com>
2024-02-20 15:49:29 +0500

    feat: configure commit linting and adjusted the default port to 3000 in the Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react-swc';
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()]
+  plugins: [react(), eslintPlugin()],
+  server: { port: 3000 },
 });
