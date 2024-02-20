65526e07b7cfcbc942518bfa5d0a310c25d180b0
imohib168 <imohib168@gmail.com>
2024-02-20 16:16:02 +0500

    feat: configure JS paths in tsconfig to resolve imports using '@/path/to/folder'

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './app';
+import App from '@/app';
 
-import './index.css';
+import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react-swc';
 import eslintPlugin from 'vite-plugin-eslint';
+import jsconfigPaths from 'vite-jsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
   server: { port: 3000 },
+  plugins: [react(), eslintPlugin(), jsconfigPaths()],
+  resolve: { alias: { '@': new URL('./src', import.meta.url).pathname } },
 });
